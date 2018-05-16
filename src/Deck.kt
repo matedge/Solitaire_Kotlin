3ea7f3e180aefa86feab63aca9c5b2861cf01ec7
matedge <mat.edge@gmail.com>
2018-05-16 13:47:06 +1000

    Added Deck along with drawCard and Reset options

---
+++ src/Deck.kt
@@ -0,0 +1,22 @@
+import java.util.*
+
+class Deck {
+
+    val cards: Array<Card> = Array(52, { Card(it % 13, getSuit(it)) })
+
+    var cardsInDeck: MutableList<Card> = cards.toMutableList()
+
+    fun drawCard(): Card = cardsInDeck.removeAt(0)
+
+    fun reset() {
+        cardsInDeck = cards.toMutableList()
+        Collections.shuffle(cardsInDeck)
+    }
+
+    private fun getSuit(i: Int): String = when(i/13) {
+        0 -> "Clubs"
+        1 -> "Diamonds"
+        2 -> "Hearts"
+        else  -> "Spades"
+    }
+}
